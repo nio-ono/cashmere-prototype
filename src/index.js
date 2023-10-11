022046f102af47c28f350c7a7a254c79c5aa335b
Nio Ono <me@nioono.com>
2023-10-11 01:50:43 +0000

    refactor + resizing / dynamic grid

--- src/index.js
+++ src/index.js
@@ -1,72 +1,81 @@
 import * as THREE from 'three';
 
-// Scene, Camera, and Renderer setup
+// === GLOBAL PARAMETERS ===
+
+const cameraZPosition = 50;
+const gridSpacing = 10;
+const dotBaseSize = 0.1;
+const waveSpeed = .5;
+const waveIntensity = .5;
+
+const vertices = [];
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = cameraZPosition;
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Parameters for the dot matrix grid
-const gridSize = 500; 
-const dotBaseSize = .001;
+let gridRows = Math.floor(window.innerWidth / gridSpacing);
+let gridCols = Math.floor(window.innerHeight / gridSpacing);
 
-// Generate grid of dots
-const geometry = new THREE.BufferGeometry();
-const vertices = [];
-
-for (let i = -gridSize; i <= gridSize; i++) {
-    for (let j = -gridSize; j <= gridSize; j++) {
-        vertices.push(i, j, 0);
+function createGrid() {
+    vertices.length = 0;
+    const halfWidth = (gridRows * gridSpacing) / 2;
+    const halfHeight = (gridCols * gridSpacing) / 2;
+    for (let i = -halfWidth; i <= halfWidth; i += gridSpacing) {
+        for (let j = -halfHeight; j <= halfHeight; j += gridSpacing) {
+            vertices.push(i, j, 0);
+        }
     }
+    return new THREE.Float32BufferAttribute(vertices, 3);
 }
 
-geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+const geometry = new THREE.BufferGeometry();
+geometry.setAttribute('position', createGrid());
 
-// Custom shader to adjust individual dot sizes
 const shaderMaterial = new THREE.ShaderMaterial({
     uniforms: {
         time: { value: 0 },
-        size: { value: dotBaseSize }
+        pointSize: { value: 10.0 }
     },
     vertexShader: `
+        precision highp float;
         uniform float time;
-        attribute float scale;
+        in float scale;
+        
         void main() {
             vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-            gl_PointSize = scale * ( 300.0 / -mvPosition.z );
+            gl_PointSize = 5.0;
             gl_Position = projectionMatrix * mvPosition;
         }
     `,
     fragmentShader: `
+        precision highp float;
+        
         void main() {
             gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 );
         }
     `,
-    transparent: true
+    transparent: true,
+    depthTest: false,
 });
 
 const particles = new THREE.Points(geometry, shaderMaterial);
 scene.add(particles);
 
-// Parameters for the wave animation
-const waveSpeed = 0.05;
-const waveIntensity = 0.5;
-
 let time = 0;
-const scales = new Float32Array(vertices.length / 3).fill(1);  // Initialize all scales to 1
+const scales = new Float32Array(gridRows * gridCols).fill(5.0);
 geometry.setAttribute('scale', new THREE.BufferAttribute(scales, 1));
 
-// Animation loop
 function animate() {
     time += waveSpeed;
 
     for (let i = 0, j = 0; i < scales.length; i++, j += 3) {
         const x = vertices[j];
         const y = vertices[j + 1];
-        scales[i] = 1 + waveIntensity * Math.sin(0.1 * x + time) * Math.sin(0.1 * y + time);
     }
     
     particles.geometry.attributes.scale.needsUpdate = true;
@@ -75,5 +84,20 @@ function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
+
+window.addEventListener('resize', () => {
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
+
+    renderer.setSize(newWidth, newHeight);
+    camera.aspect = newWidth / newHeight;
+    camera.updateProjectionMatrix();
+
+    gridRows = Math.floor(newWidth / gridSpacing);
+    gridCols = Math.floor(newHeight / gridSpacing);
+
+    geometry.setAttribute('position', createGrid());
+    geometry.attributes.scale.array = new Float32Array(gridRows * gridCols).fill(1);
+});
 
 animate();
